f869a9d975c23a597752976d5850de1a753b92c3
TheImpatient <benjvdv@gmail.com>
2015-02-24 19:58:25 +0100

    Rudimentary html parsing, many TODO's

--- RoosterCrawler/Program.cs
+++ RoosterCrawler/Program.cs
@@ -16,40 +16,63 @@ namespace RoosterCrawler
             Console.WriteLine(foo);
             Console.Read();
         }
-        /*TODO: catch webclient errors / use html file instead / sanitise input / get around idiosyncrasy
+        //TODO: catch webclient errors / use html file instead / sanitise input / get around idiosyncrasy
         private static List<String>[] HtmlParser()
         {
-            WebClient webClient = new WebClient();
-            String page = new WebClient().DownloadString("http://misc.hro.nl/roosterdienst/webroosters/cmi/kw3/08/c/c00085.htm");
-            
-            HtmlDocument htmldoc =  new HtmlDocument();
+            //WebClient webClient = new WebClient();
+            //String page2 = new WebClient().DownloadString("http://misc.hro.nl/roosterdienst/webroosters/cmi/kw3/10/c/c00084.htm");
+
+            String page = System.IO.File.ReadAllText("c00084.htm").Trim();
+
+            HtmlDocument htmldoc = new HtmlDocument();
             htmldoc.LoadHtml(page);
 
+            //TODO: Filter only relevant tabledata
             List<HtmlNode> table = htmldoc.DocumentNode.SelectNodes("//table").First().SelectNodes("tr").Descendants("td").Skip(1).ToList();
 
-            int i = 0;
 
-            List<String>[] strArr = new List<String>[30];
+            //Lists all elements innerText per row
+            List<String>[] strArr = new List<String>[37];
+            for (int i = 0; i < strArr.Length; i++) { strArr[i] = new List<string>(); }
 
+            //Lists all elements innerText
             List<String> strlist = new List<String>();
+
+            int j = 0;
 
             foreach (HtmlNode h in table)
             {
-                var bla = (int)((i) / 6);
-                strArr[(int)((i) / 6)].Add(h.InnerText);
+                if (h.InnerText != "") { strArr[(int)((j) / 6)].Add(h.InnerText); }
+                else { strArr[(int)((j) / 6)].Add("EMPTY"); }
+
                 strlist.Add(h.InnerText);
                 HtmlAttribute rowspan = h.Attributes.Where(x => x.Name.Equals("rowspan")).SingleOrDefault();
 
-                String rowvalue = rowspan != null ? rowspan.Value : "EMPTY";
-                i++;
-                Console.WriteLine("~~~~~~I:" + i);
-                
+                int rowvalue = 0;
+
+                //TODO: TryCatch parse error with non-numeric value
+                try
+                {
+                    rowvalue = int.Parse(rowspan != null ? rowspan.Value : "EMPTY");
+                }
+                catch (FormatException e)
+                {
+                    //Chomp
+                }
+
+                if (rowvalue >= 4)
+                {
+                    for (int i = 1; i < rowvalue / 2; i++)
+                    {
+                        strArr[(int)((j) / 6) + i].Add(h.InnerText);
+                    }
+                }
+
+                j++;
+                Console.WriteLine("~~~~~~I:" + rowvalue);
+                //Console.WriteLine("~~~~~~I:" + j);
             }
-
             return strArr;
-
         }
-        */
-
     }
 }
