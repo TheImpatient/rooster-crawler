b9ef55e882a3b588c96807b8b7e7cd490e5e6e5e
Thijs Vredenbregt <thijs+github@vredenbregt.net>
2015-03-04 10:13:51 +0100

    added database communications

--- RoosterCrawler/Program.cs
+++ RoosterCrawler/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace RoosterCrawler
 {
@@ -14,7 +15,45 @@ namespace RoosterCrawler
 
         public void Main()
         {
-            _schedule = new Schedule();
+            //_schedule = new Schedule();
+
+
+            const string connectionString = @"Server=195.8.208.128;Port=3351;Database=rooster;Uid=crawler;Pwd=g!MqEFCXbVK0P3hv^Jy5;";
+            MySqlConnection connection = null;
+            MySqlDataReader reader = null;
+            string deelnemers = String.Empty;
+
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+                const string query = "SELECT * FROM les";
+                var sqlCommand = new MySqlCommand(query, connection);
+                reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    deelnemers = (string)reader.GetValue(1);
+                    
+                }
+            }
+            catch (MySqlException err)
+            {
+                Console.WriteLine("Error: " + err.ToString());
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+
 
             //Week foo = DataParser();
             //Console.WriteLine(foo);
