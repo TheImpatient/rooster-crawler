a20fef7feac3d22b34812d0f6929d10545307fee
Thijs Vredenbregt <thijs+github@vredenbregt.net>
2015-03-16 21:08:45 +0100

    test voor cibs

--- HeartbeatMonitor/Credentials.cs
+++ HeartbeatMonitor/Credentials.cs
@@ -8,7 +8,6 @@ namespace HeartbeatMonitor
     public class Credentials
     {
         public const string ConnectionString = "Server=195.8.208.128;Port=3351;Database=rooster;Uid=crawler;Pwd=g!MqEFCXbVK0P3hv^Jy5;";
-
         public static string GetCredentials()
         {
             return ConnectionString;
